1e19ba5cc8ef49b8160cf9b6bdef4f51842ca12f
Ana Lam <alam@barnard.edu>
2020-11-05 12:31:50 -0500

    build scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// some description coming soon
+
+// initialize map
+const map = L.map("mapdiv", {zoomControl: false});
+
+// starting point!
+const newyorkcity = L.latLng([40.712207, -73.939589]);
+const zoomLevel = 10.3;
+
+// tile layer for the map
+map.setView(newyorkcity, zoomLevel);
+L.tileLayer.provider("Esri.WorldImagery").addTo(map);
+
+// marker for our starting point
+const grandstMarker = L.marker(newyorkcity).addTo(map);
+const md = markdownit({html: true}).use(markdownitFootnote);
+grandstMarker.bindPopup(md.render("### Hello from the [Grand St Station](http://web.mta.info/nyct/service/lline.htm)!"));
