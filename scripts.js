602face6083fae1ad299c6d806474c341211e39c
Ana Lam <alam@barnard.edu>
2020-11-10 13:38:53 -0500

    boot leaflet, add card text

--- scripts.js
+++ scripts.js
@@ -15,3 +15,15 @@ L.tileLayer.provider("Esri.WorldImagery").addTo(map);
 const grandstMarker = L.marker(newyorkcity).addTo(map);
 const md = markdownit({html: true}).use(markdownitFootnote);
 grandstMarker.bindPopup(md.render("### Hello from the [Grand St Station](http://web.mta.info/nyct/service/lline.htm)!"));
+
+// use jQuery to manipulate html elements
+
+$("#card-header-text").html("<strong>expenv-ent</strong>");
+
+// point to body.md
+
+$.ajax({ url: "body.md",
+  success(bodyMarkdown) {
+    $("#outlet-card-body").html(md.render(bodyMarkdown));
+  }
+});
