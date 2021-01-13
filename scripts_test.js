a6b6c7f9c26d059035e5718068a44922d5f10402
Ana Lam <alam@barnard.edu>
2021-01-13 16:57:37 -0500

    add console.log

--- scripts_test.js
+++ scripts_test.js
@@ -3,37 +3,39 @@ const md = markdownit({html: true}).use(markdownitFootnote);
 var datalayer = L.layerGroup();
 
 $.getJSON("cd.geojson", geodata => {
-  L.geoJSON(geodata, {
-    style() {
-      return {
-        color: "#000000",
-        weight: 1,
-        fillOpacity: 0.0
-      };
-    }
-  }).addTo(datalayer);
+    L.geoJSON(geodata, {
+     style() {
+       return {
+         color: "#000000",
+         weight: 1,
+         fillOpacity: 0.0
+       };
+     }
+   }).addTo(datalayer);
 });
 
 // fine particulate matter layer with choropleth feature
 $.getJSON("pm25.geojson", function(data){
-  L.geoJSON( data , {
-  style: function(feature){
-    var fillColor,
-        DataValue = feature.properties.DataValue;
-    if ( DataValue > 11 ) fillColor = "#006837";
-    else if ( DataValue > 10 ) fillColor = "#31a354";
-    else if ( DataValue > 8.4 ) fillColor = "#78c679";
-    else if ( DataValue > 7.5 ) fillColor = "#c2e699";
-    else if ( DataValue > 0 ) fillColor = "#ffffcc";
-    else fillColor = "#f7f7f7";
-    return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .6 };
-      },
-      onEachFeature: function( feature, layer ){
-        layer.bindPopup( "<strong>" + feature.properties.Name + "</strong><br/>" + feature.properties.DataValue + " PM2.5" );
-      }
-    }).addTo(datalayer);
-  });
+   console.log(data);
+   L.geoJSON( data , {
+   style: function(feature){
+     var fillColor,
+         DataValue = feature.properties.DataValue;
+     if ( DataValue > 11 ) fillColor = "#006837";
+     else if ( DataValue > 10 ) fillColor = "#31a354";
+     else if ( DataValue > 8.4 ) fillColor = "#78c679";
+     else if ( DataValue > 7.5 ) fillColor = "#c2e699";
+     else if ( DataValue > 0 ) fillColor = "#ffffcc";
+     else fillColor = "#f7f7f7";
+     return { color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .6 };
+       },
+       onEachFeature: function( feature, layer ){
+         layer.bindPopup( "<strong>" + feature.properties.Name + "</strong><br/>" + feature.properties.DataValue + " PM2.5" );
+       }
+     }).addTo(datalayer);
+});
 
+console.log(datalayer);
 
 var satellite = L.tileLayer.provider("Esri.WorldImagery");
 
